9a59d22309c29ce7b09fda2d7e5824e792c8f217
edson.neto <edson.neto@IW1065EDSONNETO>
2024-07-09 16:25:50 -0400

    Adicionar arquivos de projeto.

---
+++ src/Journey.Application/UseCases/Trips/GetAll/GetAllUseCase.cs
@@ -0,0 +1,31 @@
+﻿using Journey.Communication.Responses;
+using Journey.Infrastructure;
+
+namespace Journey.Application.UseCases.Trips.GetAll
+{
+    public class GetAllUseCase
+    {
+        public ResponseTripsJson Execute()
+        {
+         
+
+            var dbContext = new JourneyDbContext();
+
+            var trips = dbContext.Trips.ToList();
+
+            return new ResponseTripsJson
+            {
+                Trips = trips.Select(trip => new ResponseShortTripJson
+                {
+                    Id = trip.Id,
+                    Name = trip.Name,
+                    StartDate = trip.StartDate,
+                    EndDate = trip.EndDate
+                   
+                }).ToList()
+            };
+        }
+    }
+}
+
+
---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure
+{
+    public class JourneyDbContext : DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\edson.neto\\Documents\\Projetos\\JourneyDatabase.db");
+        }
+    }
+}
